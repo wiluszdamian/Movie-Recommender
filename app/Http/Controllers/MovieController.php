71101f322ca10afad6d4bbf8725928f791987702
wiluszdamian <damians.wilusz@gmail.com>
2023-04-10 22:31:08 +0200

    ADD: PHPDoc for MovieController::index()

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\TmdbApiService;
+use Illuminate\View\View;
 
 class MovieController extends Controller
 {
+    /**
+     * Show details of a specific media using its ID.
+     *
+     * @param  TmdbApiService  $tmdbApiService The TMDb API service instance.
+     * @param  string  $id The ID of the media.
+     * @return View The view displaying the details of the media.
+     */
     public function show(TmdbApiService $tmdbApiService, string $id)
     {
         $details = $tmdbApiService->getMediaDetails('movie', (int) $id);
